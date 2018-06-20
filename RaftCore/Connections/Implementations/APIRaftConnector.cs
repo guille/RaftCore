59d3f86a6bc801653b42bcb1b4b67cfb666f393a
guille <guille@users.noreply.github.com>
2018-06-20 23:20:07 +0200

    Created API connector, renamed StateMachine interface

---
+++ RaftCore/Connections/Implementations/APIRaftConnector.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using RaftCore;
+using RaftCore.Connections;
+using RaftCore.Components;
+
+namespace RaftCore.Connections.Implementations {
+    public class APIRaftConnector : IRaftConnector {
+        // Connects to a web API given by a base URL
+        // TODO: Document endpoints
+
+        public uint NodeId { get; private set; }
+        private string baseURL { get; set; }
+
+        public APIRaftConnector(uint nodeId, string baseURL) {
+            this.NodeId = nodeId;
+            this.baseURL = baseURL;
+        }
+
+        public void MakeRequest(String command) {
+            // convert params to json
+            // make POST <baseurl>/makerequest
+        }
+        
+        public Result<bool> RequestVote(int term, uint candidateId, int lastLogIndex, int lastLogTerm) {
+            // convert params to json
+            // make POST <baseurl>/requestvote
+            // parse response into a Result object
+            return new Result<bool>(true, 1);
+        }
+
+        public Result<bool> AppendEntries(int term, uint leaderId, int prevLogIndex, int prevLogTerm, List<LogEntry> entries, int leaderCommit) {
+            // convert params to json
+            // make POST <baseurl>/appendentries
+            // parse response into a Result object
+            return new Result<bool>(true, 1);
+        }
+
+        public void TestConnection() {
+            // make POST <baseurl>/test
+        }
+
+        public void Run() {
+            // make POST <baseurl>/run
+        }
+
+    }
+}
---
+++ RaftCore/StateMachine/IRaftStateMachine.cs
@@ -0,0 +1,22 @@
+﻿namespace RaftCore.StateMachine {
+    public interface IRaftStateMachine {
+        /// <summary>
+        /// Applies a command to the state machine. The State Machine will silently ignore bad commands.
+        /// </summary>
+        /// <param name="command">Command to apply</param>
+        void Apply(string command);
+
+        /// <summary>
+        /// Finds the string representation of the state machine, limited by the given command.
+        /// </summary>
+        /// <param name="param">Limits, filters or specifies the information to retrieve.</param>
+        /// <returns>A string containing the specified representation of the state machine</returns>
+        string RequestStatus(string param);
+
+        /// <summary>
+        /// Used to determine the broadcast time.
+        /// The method must implement a dummy request to the state machine, in order to correctly approximate the broadcast time.
+        /// </summary>
+        void TestConnection();
+    }
+}
